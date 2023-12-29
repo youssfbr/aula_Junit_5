72beb31a550a2cc1fdfaf60349e7a443e7d04831
Alisson Youssf <youssfbr@gmail.com>
2023-12-29 18:58:17 -0300

    Testes para método withdraw

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -47,4 +47,27 @@ public class AccountTests {
         Assertions.assertTrue(expectedValue == acc.getBalance());
         Assertions.assertTrue(result == initialBalance);
     }
+
+    @Test
+    public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+
+        // Preparar os dados
+        Account acc = AccountFactory.createAccount(800.0);
+
+        // Ação
+        acc.withdraw(500.0);
+
+        Assertions.assertEquals(300.0, acc.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+
+            Account acc = AccountFactory.createAccount(800.0);
+
+            acc.withdraw(801.0);
+        });
+    }
 }
